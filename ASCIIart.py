42ac897a63db326cb5d18dd8dc1f737c43acafd1
nox <nox@noxsoftware.pl>
2025-06-11 22:56:30 +0200

    adjustements

--- ASCIIart.py
+++ ASCIIart.py
@@ -1,7 +1,7 @@
 import cv2
 
 class ASCIIart:
-    def __init__(self, color_balance=1.0, charset="@%#*+=-:. ", width=80, height=40):
+    def __init__(self, color_balance=0.5, charset="#@*:. ", width=80, height=40):
         self.image = None
         self.ascii_art = None
         self.color_balance = color_balance
@@ -18,11 +18,11 @@ class ASCIIart:
         if self.image is None:
             raise ValueError("Image not loaded. Please load an image first.")
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        self.image = cv2.GaussianBlur(self.image, (5, 5), 0)
+        self.image = cv2.addWeighted(self.image, 1.5, self.image, -0.5, 0)
+        self.image = cv2.convertScaleAbs(self.image, alpha=1.5, beta=0)
         self.image = cv2.resize(self.image, (self.width, self.height))
         self.image = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
-        self.image = cv2.normalize(self.image, None, 0, 255, cv2.NORM_MINMAX)
-        self.image = cv2.convertScaleAbs(self.image, alpha=self.color_balance)
-        self.image = 255 - self.image
 
     def generate_ascii_art(self):
         if self.image is None:
@@ -34,6 +34,12 @@ class ASCIIart:
             line = ""
             for x in range(width):
                 pixel_value = self.image[y, x]
+                pixel_value = int(pixel_value * self.color_balance)
+                if pixel_value < 0:
+                    pixel_value = 0
+                elif pixel_value > 255:
+                    pixel_value = 255
+                pixel_value = int(255 * (pixel_value / 255) ** 0.5)
                 index = int(pixel_value / 255 * (len(self.charset) - 1))
                 if index < 0:
                     index = 0
@@ -47,7 +53,7 @@ class ASCIIart:
         return self.ascii_art
 
 if __name__ == "__main__":
-    ascii_art = ASCIIart(color_balance=1.0, charset="@%#*+=-:. ", width=80, height=40)
+    ascii_art = ASCIIart(color_balance=0.7)
     try:
         path = input("Enter the path to the image: ")
         ascii_art.load_image(path)
